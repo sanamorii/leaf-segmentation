cee1f73a90a1040bd26078e86295ce81fb1355ca
Qiang Cai <contact@sanamorii.com>
2025-08-07 13:56:03 +0100

    exclude background class

--- loss/cedice.py
+++ loss/cedice.py
@@ -5,8 +5,8 @@ import segmentation_models_pytorch.losses as smp_losses
 class CEDiceLoss(nn.Module):
     def __init__(self, ce_weight=0.5, dice_weight=0.5):
         super().__init__()
-        self.ce = nn.CrossEntropyLoss(ignore_index=255)  # Modify if you use a different ignore index
-        self.dice = smp_losses.DiceLoss(mode='multiclass')  # For multiclass segmentation
+        self.ce = nn.CrossEntropyLoss(ignore_index=0) 
+        self.dice = smp_losses.DiceLoss(mode='multiclass', classes=[1,2,3,4])
         self.ce_weight = ce_weight
         self.dice_weight = dice_weight
 
